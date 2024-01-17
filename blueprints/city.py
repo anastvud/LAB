080e0710536273c1f5f6e4f4111e99813c5ec44c
Anastasiia Vudvud <anastasiiavudvud@gmail.com>
2024-01-17 19:14:45 +0100

    post, put, delete works

--- blueprints/city.py
+++ blueprints/city.py
@@ -5,7 +5,7 @@ from marshmallow import ValidationError
 
 
 from db_utils import errors, create_entry, update_entry
-from schemas import CityData, CityReq
+from schemas import CityData
 from models import City
 from db_utils import session
 
@@ -14,35 +14,40 @@ city_blueprint = Blueprint("city", __name__)
 
 @city_blueprint.route("/city", methods=["POST", "GET"])
 def city_():
-    if request.method == "POST":
-        request_data = {
-            "name": request.form["name"],
-            "country": request.form["country"],
-            "foundation_year": int(request.form["year"]),
-        }
-        json_data = json.dumps(request_data)
-        # print(json_data)
-        if not json_data:
-            return errors.bad_request
-        try:
-            subject_data = CityData().loads(json_data)
-            print(subject_data)
-        except ValidationError as err:
-            return err.messages, 422
-        new_subject = create_entry(City, **subject_data)
-        return jsonify(CityReq().dump(new_subject))
-
     if request.method == "GET":
         city_list = session.query(City).all()
         data = CityData().dump(city_list, many=True)
         return render_template("city.html", cities=data)
 
+    if request.method == "POST":
+        if 'delete_id' in request.form:
+            city = session.query(City).filter_by(idcity=int(request.form["delete_id"])).first()
+            session.delete(city)
+            session.commit()
+            return redirect(url_for('city.city_'))
+        else:
+            request_data = {
+                "name": request.form["name"],
+                "country": request.form["country"],
+                "foundation_year": int(request.form["year"]),
+            }
+            json_data = json.dumps(request_data)
+            if not json_data:
+                return errors.bad_request
+            try:
+                subject_data = CityData().loads(json_data)
+                print(subject_data)
+            except ValidationError as err:
+                return err.messages, 422
+            create_entry(City, **subject_data)
+            return redirect(url_for('city.city_'))
 
 @city_blueprint.route("/city/<int:id>", methods=["GET", "POST", "DELETE"])
 def city_by_id(id):
     city = session.query(City).filter_by(idcity=id).first()
     if not city:
         return errors.not_found
+
     if request.method == "GET":
         data = CityData().dump(city)
         return render_template("city_id.html", city=data)
@@ -60,8 +65,6 @@ def city_by_id(id):
             request_data["foundation_year"] = int(request.form["year"])
 
         json_data = json.dumps(request_data)
-        # print(json_data)
-        # json_data = request.json
         if not json_data:
             return errors.bad_request
         try:
@@ -71,12 +74,6 @@ def city_by_id(id):
         updated_city = update_entry(city, **data)
         data = CityData().dump(updated_city)
         return render_template("city_id.html", city=data)
-
-    if request.method == "DELETE":
-        session.delete(city)
-        session.commit()
-        return errors.success
-    return redirect(url_for("city_"))
 
 
 @city_blueprint.route("/city/<string:country>", methods=["GET"])
